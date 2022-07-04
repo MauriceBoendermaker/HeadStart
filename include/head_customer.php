ea96fce3852192cde7364e9028df5d804f8638c0
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-04 20:59:23 +0200

    Created head section

---
+++ include/head_customer.php
@@ -0,0 +1,43 @@
+<?php
+//using the Database class
+include_once '../config.php';
+include_once '../database/database.php';
+
+function endsWith($string, ...$endString)
+{
+	foreach ($endString as $end) {
+		$len = strlen($end);
+		if ($len == 0) {
+			return true;
+		}
+		if (substr($string, -$len) === $end) {
+			return true;
+		}
+	}
+	return false;
+}
+
+$db = new database\Database($db_host, $db_user, $db_pass, $db_name, $db_port);
+$tests = $db->getTests();
+
+$id = -1;
+$test = $db->getTestByID($id);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title><?php foreach ($tests as $testData) { echo "{$testData->getNaam()}"; } ?> | HeadStart</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">	<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+	<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css" />
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
