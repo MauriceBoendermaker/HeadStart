db4f48165075ed1b3c53eff40ce79efcedfe0c9d
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-06 08:31:33 +0200

    Created editor.php and made first steps to build the columns

---
+++ customer/editor.php
@@ -0,0 +1,10 @@
+<?php include "../include/head_customer.php"; ?>
+<body>
+	<div class="container float-start">
+		<div class="row">
+			<div class="col-sm-8">col-sm-8</div>
+			<div class="col-sm-4">col-sm-4</div>
+		</div>
+	</div>
+</body>
+</html>
