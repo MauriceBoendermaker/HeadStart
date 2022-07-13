bda2ab30e24d175ce914e464b0d486f1ae6e9d9a
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-13 23:25:36 +0200

    Added line breaks to make code cleaner

--- customer/editor.php
+++ customer/editor.php
@@ -14,11 +14,20 @@ $templateGetFunction = "get" . ucfirst($templateName);
 $templateDataAll = $db->getVanilla526();
 
 $configFile = $templateName . "_config.json";
+
+
+
+
+
 foreach ($characters as $key => $value1) {
 	echo $value1["theme"] . '<br>';
 }
 
+
+
+
 foreach ($templateDataAll as $templateData) {
+
 ?>
 <body>
 </body>
