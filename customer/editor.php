00347a30679396d3efc7605b5fddc51f3dc2ea12
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-13 23:22:30 +0200

    Added function to get template name from url

--- customer/editor.php
+++ customer/editor.php
@@ -3,6 +3,8 @@
 $db = new database\Database($db_host, $db_user, $db_pass, $db_name, $db_port);
 include_once '../config.php';
 
+$templateName = $_GET['template'];
+
 
 foreach ($templateDataAll as $templateData) {
 ?>
