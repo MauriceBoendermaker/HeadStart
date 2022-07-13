a849304f0637f8b21e1243c7a8d1013678f233cb
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-13 23:24:45 +0200

    Added functions to dynamically load database classes

--- customer/editor.php
+++ customer/editor.php
@@ -8,6 +8,11 @@ $templateName = $_GET['template'];
 $templatePath = "../templates/" . $templateName;
 
 include_once $templatePath . '/database/database_'. $templateName . '.php';
+
+$db = new \vanilla526\database_vanilla526($db_host, $db_user, $db_pass, $db_name, $db_port);
+$templateGetFunction = "get" . ucfirst($templateName);
+$templateDataAll = $db->getVanilla526();
+
 $configFile = $templateName . "_config.json";
 foreach ($characters as $key => $value1) {
 	echo $value1["theme"] . '<br>';
