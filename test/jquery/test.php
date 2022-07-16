f1b169d7bf73ab1f201aa46dee1c65d2b5de0226
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-16 23:31:51 +0200

    Test file to retrieve data dynamically using jQuery

--- test/jquery/test.php
+++ test/jquery/test.php
@@ -6,6 +6,13 @@ $tmpArray = array(array("one",array(1,2,3)),array("two",array(4,5,6)),array("thr
 
 $testData = json_decode($jsonData);
 
+include_once '../../config.php';
+include_once "../../templates/vanilla526/database/database_vanilla526.php";
+$db = new \vanilla526\database_vanilla526($db_host, $db_user, $db_pass, $db_name, $db_port);
+$templateDataAll = $db->getVanilla526();
+
+$array1 = json_decode(json_encode((array)$jsonData), true);
+
 //var_dump($testData);
 
 $s=array('a','b','c','d','e','f');
@@ -16,12 +23,27 @@ $s=array('a','b','c','d','e','f');
 $characters = json_decode($jsonData, 1);
 
 
-foreach ((array)$jsonData["id"] as $value) {
-//	echo $value[0];
-	foreach((array)$value["id"] as $key1) {
-		echo $key1;
-	}
+//$i = 0;
+//foreach ($array1 as $somekey=>$someval) {
+//	echo $array1[$i];
+//	$i++;
+//}
+
+
+$someCounted = count($array1);
+for($i = 0; $i < $someCounted; $i++){
+	echo $array1[$i];
 }
 
 
+foreach ($templateDataAll as $templateData) {
+
+	// dynamically create the html for the page based on the template config file
+	$array = array();
+
+	foreach ($templateData as $key => $value) {
+		$array[$key] = $value;
+	}
+
+}
 ?>
