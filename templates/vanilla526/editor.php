79cd9cb2eee68f1b32631928e9a4ffb4fc853240
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-17 20:00:32 +0200

    Create new connection to database

--- templates/vanilla526/editor.php
+++ templates/vanilla526/editor.php
@@ -5,4 +5,8 @@ use vanilla526\database_vanilla526;
 include_once '../../config.php';
 include_once "../../include/head_customer.php";
 include_once "database/database_vanilla526.php";
+
+$db = new database_vanilla526($db_host, $db_user, $db_pass, $db_name, $db_port);
+$templateDataAll = $db->getVanilla526();
+
 ?>
