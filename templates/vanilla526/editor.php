d4d552068bde435905afdad3ef39b928f6bbcb83
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-18 21:33:52 +0200

    Added function to post slider data to database

--- templates/vanilla526/editor.php
+++ templates/vanilla526/editor.php
@@ -39,6 +39,11 @@ if (isset($_POST['save_footer'])) {
 	home();
 }
 
+if (isset($_POST['save_carousel'])) {
+	$db->setVanilla526Carousel($_POST["CarouselImg1"], $_POST["CarouselText1"], $_POST["CarouselSub1"], $_POST["CarouselImg2"], $_POST["CarouselText2"], $_POST["CarouselSub2"], $_POST["CarouselImg3"], $_POST["CarouselText3"], $_POST["CarouselSub3"], $_POST["CarouselImg4"], $_POST["CarouselText4"], $_POST["CarouselSub4"], $_POST["CarouselImg5"], $_POST["CarouselText5"], $_POST["CarouselSub5"], $_POST["CarouselImg6"], $_POST["CarouselText6"], $_POST["CarouselSub6"], $_POST["CarouselImg7"], $_POST["CarouselText7"], $_POST["CarouselSub7"], $_POST["CarouselImg8"], $_POST["CarouselText8"], $_POST["CarouselSub8"]);
+	home();
+}
+
 	home();
 }
 
