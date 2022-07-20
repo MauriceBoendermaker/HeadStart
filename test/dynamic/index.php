01aa72d70bc72a661d0389c96ca1a0064fb6663f
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-20 23:11:23 +0200

    Added index page to test dynamic tables

--- test/dynamic/index.php
+++ test/dynamic/index.php
@@ -16,5 +16,34 @@
 	</style>
 </head>
 <body>
+
+<?php
+$theme = intval($_GET['theme']);
+
+$con = mysqli_connect("localhost","root","","headstart");
+if (!$con) {
+	die('Could not connect: ' . mysqli_error($con));
+}
+
+mysqli_select_db($con,"headstart");
+$sql="SELECT * FROM vanilla526 WHERE id = '".$theme."'";
+$result = mysqli_query($con,$sql);
+
+echo "<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+<th>Age</th>
+</tr>";
+while($row = mysqli_fetch_array($result)) {
+	echo "<tr>";
+	echo "<td>" . $row['title'] . "</td>";
+	echo "<td>" . $row['seo_copy'] . "</td>";
+	echo "<td>" . $row['seo_lang'] . "</td>";
+	echo "</tr>";
+}
+echo "</table>";
+mysqli_close($con);
+?>
 </body>
 </html>
