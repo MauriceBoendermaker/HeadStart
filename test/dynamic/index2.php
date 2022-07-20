e3a7937c4445a5c912ada4de4382541179390f14
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-20 23:11:42 +0200

    Added 2nd page to test dynamic tables

---
+++ test/dynamic/index2.php
@@ -0,0 +1,43 @@
+<?php
+//
+//$theme = intval($_GET['theme']);
+//
+//?>
+<!---->
+<!--<html>-->
+<!--<head>-->
+<!--	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+<!--	<script>-->
+<!--        function showUser(str) {-->
+<!--            if (str == "") {-->
+<!--                document.getElementById("txtHint").innerHTML = "";-->
+<!--                return;-->
+<!--            } else {-->
+<!--                var xmlhttp = new XMLHttpRequest();-->
+<!--                xmlhttp.onreadystatechange = function() {-->
+<!--                    if (this.readyState == 4 && this.status == 200) {-->
+<!--                        document.getElementById("txtHint").innerHTML = this.responseText;-->
+<!--                    }-->
+<!--                };-->
+<!--                xmlhttp.open("GET","index.php?theme="+str,true);-->
+<!--                xmlhttp.send();-->
+<!--            }-->
+<!--        }-->
+<!--	</script>-->
+<!--</head>-->
+<!--<body>-->
+<!---->
+<!--<form>-->
+<!--	<select name="users" onchange="showUser(this.value)">-->
+<!--		<option value="">Select a person:</option>-->
+<!--		<option value="1">Peter Griffin</option>-->
+<!--		<option value="2">Lois Griffin</option>-->
+<!--		<option value="3">Joseph Swanson</option>-->
+<!--		<option value="4">Glenn Quagmire</option>-->
+<!--	</select>-->
+<!--</form>-->
+<!--<br>-->
+<!--<div id="txtHint"><b>Person info will be listed here...</b></div>-->
+<!---->
+<!--</body>-->
+<!--</html>-->
