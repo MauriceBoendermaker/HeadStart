f50c9d8f47fbafe899548a55933373a9b1e0dde9
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-22 11:03:32 +0200

    Added test function to highlight Headline in iFrame

--- templates/vanilla526/editor.js
+++ templates/vanilla526/editor.js
@@ -1,1 +1,14 @@
+function myFunction() {
+    var iframe = document.getElementById("frame1");
+    var elmnt = iframe.contentWindow.document.getElementById("headline");
+    elmnt.style.position = "relative";
+    elmnt.style.boxShadow = "0 0 0 1600px rgba(0,0,0,0.65)";
+}
+
+function myFunction1() {
+    var iframe = document.getElementById("frame1");
+    var elmnt = iframe.contentWindow.document.getElementById("headline");
+    // elmnt.style.position = "auto";
+    elmnt.style.boxShadow = "none";
+}
 
