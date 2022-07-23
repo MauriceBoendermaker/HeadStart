a416aa5aa19b4561b5908c282a77f1321debc1d2
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-23 23:01:49 +0200

    Renamed function to appropriate name

--- templates/vanilla526/editor.js
+++ templates/vanilla526/editor.js
@@ -1,11 +1,12 @@
-function myFunction() {
+// Headline
+function showHeadline() {
     var iframe = document.getElementById("frame1");
     var elmnt = iframe.contentWindow.document.getElementById("headline");
     elmnt.style.position = "relative";
     elmnt.style.boxShadow = "0 0 0 1600px rgba(0,0,0,0.65)";
 }
 
-function myFunction1() {
+function delHeadline() {
     var iframe = document.getElementById("frame1");
     var elmnt = iframe.contentWindow.document.getElementById("headline");
     // elmnt.style.position = "auto";
