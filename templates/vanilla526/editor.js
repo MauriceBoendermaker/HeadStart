9c2598317212819dc3149467856dcd8deb8209e1
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-23 23:04:33 +0200

    Added new hover function for P2HeaderText

--- templates/vanilla526/editor.js
+++ templates/vanilla526/editor.js
@@ -83,3 +83,20 @@ function delP2Header() {
     elmnt.style.position = "static";
 }
 
+// P2HeaderText
+function showP2HeaderText() {
+    var iframe = document.getElementById("frame1");
+    var elmnt = iframe.contentWindow.document.getElementById("show-P2HeaderText");
+    elmnt.style.position = "relative";
+    elmnt.style.borderRadius = "25px";
+    elmnt.style.boxShadow = "0 0 0 2000px rgba(0, 0, 0, 0.75)";
+    elmnt.style.transition = "ease-in-out box-shadow 0.5s";
+}
+
+function delP2HeaderText() {
+    var iframe = document.getElementById("frame1");
+    var elmnt = iframe.contentWindow.document.getElementById("show-P2HeaderText");
+    elmnt.style.boxShadow = "none";
+    elmnt.style.position = "static";
+}
+
