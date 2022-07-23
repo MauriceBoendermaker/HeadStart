d64a531ceda6cf6e6e39ab009cd80eb31c047b82
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-23 23:03:57 +0200

    Added new hover function for StrongSubHeadline

--- templates/vanilla526/editor.js
+++ templates/vanilla526/editor.js
@@ -15,3 +15,20 @@ function delHeadline() {
     elmnt.style.position = "static";
 }
 
+// StrongSubHeadline
+function showStrongSubHeadline() {
+    var iframe = document.getElementById("frame1");
+    var elmnt = iframe.contentWindow.document.getElementById("show-StrongSubHeadline");
+    elmnt.style.position = "relative";
+    elmnt.style.borderRadius = "25px";
+    elmnt.style.boxShadow = "0 0 0 2000px rgba(0, 0, 0, 0.75)";
+    elmnt.style.transition = "ease-in-out box-shadow 0.5s";
+}
+
+function delStrongSubHeadline() {
+    var iframe = document.getElementById("frame1");
+    var elmnt = iframe.contentWindow.document.getElementById("show-StrongSubHeadline");
+    elmnt.style.boxShadow = "none";
+    elmnt.style.position = "static";
+}
+
