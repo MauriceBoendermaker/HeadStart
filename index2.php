41b091f2418763900a17bdae6f4b8e7052f62f8c
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-07-28 21:42:11 +0200

    Re-added page setup/framework (bootstrap)

--- index2.php
+++ index2.php
@@ -174,213 +174,213 @@
 				</script>
 				<!-- side menu end -->
 			<script rel="script" src="js/footer.js"></script>
-<!--		</div>-->
-<!--	</div>-->
-<!--	<div class="row">-->
-<!--		<div class="col-md-2">-->
-<!--		</div>-->
-<!--		<div class="col-md-8">-->
-<!--			<h3 class="text-center">-->
-<!--				h3. Lorem ipsum dolor sit amet.-->
-<!--			</h3>-->
-<!--			<div class="row">-->
-<!--				<div class="col-md-6">-->
-<!--					<h2>-->
-<!--						Heading-->
-<!--					</h2>-->
-<!--					<p>-->
-<!--						Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,-->
-<!--						tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem-->
-<!--						malesuada magna mollis euismod. Donec sed odio dui.-->
-<!--					</p>-->
-<!--					<p>-->
-<!--						<a class="btn" href="#">View details »</a>-->
-<!--					</p>-->
-<!--				</div>-->
-<!--				<div class="col-md-6">-->
-<!--					<img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg">-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</div>-->
-<!--		<div class="col-md-2">-->
-<!--		</div>-->
-<!--	</div>-->
-<!--	<div class="row">-->
-<!--		<div class="col-md-2">-->
-<!--		</div>-->
-<!--		<div class="col-md-8">-->
-<!--		</div>-->
-<!--		<div class="col-md-2">-->
-<!--		</div>-->
-<!--	</div>-->
-<!--	<div class="row">-->
-<!--		<div class="col-md-2">-->
-<!--		</div>-->
-<!--		<div class="col-md-8">-->
-<!--			<div class="row">-->
-<!--				<div class="col-md-4">-->
-<!--					<ul>-->
-<!--						<li class="list-item">-->
-<!--							Lorem ipsum dolor sit amet-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Consectetur adipiscing elit-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Integer molestie lorem at massa-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Facilisis in pretium nisl aliquet-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Nulla volutpat aliquam velit-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Faucibus porta lacus fringilla vel-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Aenean sit amet erat nunc-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Eget porttitor lorem-->
-<!--						</li>-->
-<!--					</ul>-->
-<!--				</div>-->
-<!--				<div class="col-md-4">-->
-<!--					<ul>-->
-<!--						<li class="list-item">-->
-<!--							Lorem ipsum dolor sit amet-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Consectetur adipiscing elit-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Integer molestie lorem at massa-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Facilisis in pretium nisl aliquet-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Nulla volutpat aliquam velit-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Faucibus porta lacus fringilla vel-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Aenean sit amet erat nunc-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Eget porttitor lorem-->
-<!--						</li>-->
-<!--					</ul>-->
-<!--				</div>-->
-<!--				<div class="col-md-4">-->
-<!--					<ul>-->
-<!--						<li class="list-item">-->
-<!--							Lorem ipsum dolor sit amet-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Consectetur adipiscing elit-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Integer molestie lorem at massa-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Facilisis in pretium nisl aliquet-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Nulla volutpat aliquam velit-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Faucibus porta lacus fringilla vel-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Aenean sit amet erat nunc-->
-<!--						</li>-->
-<!--						<li class="list-item">-->
-<!--							Eget porttitor lorem-->
-<!--						</li>-->
-<!--					</ul>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</div>-->
-<!--		<div class="col-md-2">-->
-<!--		</div>-->
-<!--	</div>-->
-<!--	<div class="row">-->
-<!--		<div class="col-md-2">-->
-<!--		</div>-->
-<!--		<div class="col-md-8">-->
-<!--			<div class="row">-->
-<!--				<div class="col-md-4">-->
-<!--					<div class="card">-->
-<!--						<img class="card-img-top" alt="Bootstrap Thumbnail First"-->
-<!--						     src="https://www.layoutit.com/img/people-q-c-600-200-1.jpg">-->
-<!--						<div class="card-block">-->
-<!--							<h5 class="card-title">-->
-<!--								Card title-->
-<!--							</h5>-->
-<!--							<p class="card-text">-->
-<!--								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta-->
-<!--								gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.-->
-<!--							</p>-->
-<!--							<p>-->
-<!--								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>-->
-<!--							</p>-->
-<!--						</div>-->
-<!--					</div>-->
-<!--				</div>-->
-<!--				<div class="col-md-4">-->
-<!--					<div class="card">-->
-<!--						<img class="card-img-top" alt="Bootstrap Thumbnail Second"-->
-<!--						     src="https://www.layoutit.com/img/city-q-c-600-200-1.jpg">-->
-<!--						<div class="card-block">-->
-<!--							<h5 class="card-title">-->
-<!--								Card title-->
-<!--							</h5>-->
-<!--							<p class="card-text">-->
-<!--								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta-->
-<!--								gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.-->
-<!--							</p>-->
-<!--							<p>-->
-<!--								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>-->
-<!--							</p>-->
-<!--						</div>-->
-<!--					</div>-->
-<!--				</div>-->
-<!--				<div class="col-md-4">-->
-<!--					<div class="card">-->
-<!--						<img class="card-img-top" alt="Bootstrap Thumbnail Third"-->
-<!--						     src="https://www.layoutit.com/img/sports-q-c-600-200-1.jpg">-->
-<!--						<div class="card-block">-->
-<!--							<h5 class="card-title">-->
-<!--								Card title-->
-<!--							</h5>-->
-<!--							<p class="card-text">-->
-<!--								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta-->
-<!--								gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.-->
-<!--							</p>-->
-<!--							<p>-->
-<!--								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>-->
-<!--							</p>-->
-<!--						</div>-->
-<!--					</div>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</div>-->
-<!--		<div class="col-md-2">-->
-<!--		</div>-->
-<!--	</div>-->
-<!--	<div class="row">-->
-<!--		<div class="col-md-2">-->
-<!--		</div>-->
-<!--		<div class="col-md-8">-->
-<!--		</div>-->
-<!--		<div class="col-md-2">-->
-<!--		</div>-->
-<!--	</div>-->
-<!--</div>-->
-<!---->
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-8">
+			<h3 class="text-center">
+				h3. Lorem ipsum dolor sit amet.
+			</h3>
+			<div class="row">
+				<div class="col-md-6">
+					<h2>
+						Heading
+					</h2>
+					<p>
+						Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+						tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem
+						malesuada magna mollis euismod. Donec sed odio dui.
+					</p>
+					<p>
+						<a class="btn" href="#">View details »</a>
+					</p>
+				</div>
+				<div class="col-md-6">
+					<img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg">
+				</div>
+			</div>
+		</div>
+		<div class="col-md-2">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-8">
+		</div>
+		<div class="col-md-2">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-8">
+			<div class="row">
+				<div class="col-md-4">
+					<ul>
+						<li class="list-item">
+							Lorem ipsum dolor sit amet
+						</li>
+						<li class="list-item">
+							Consectetur adipiscing elit
+						</li>
+						<li class="list-item">
+							Integer molestie lorem at massa
+						</li>
+						<li class="list-item">
+							Facilisis in pretium nisl aliquet
+						</li>
+						<li class="list-item">
+							Nulla volutpat aliquam velit
+						</li>
+						<li class="list-item">
+							Faucibus porta lacus fringilla vel
+						</li>
+						<li class="list-item">
+							Aenean sit amet erat nunc
+						</li>
+						<li class="list-item">
+							Eget porttitor lorem
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<ul>
+						<li class="list-item">
+							Lorem ipsum dolor sit amet
+						</li>
+						<li class="list-item">
+							Consectetur adipiscing elit
+						</li>
+						<li class="list-item">
+							Integer molestie lorem at massa
+						</li>
+						<li class="list-item">
+							Facilisis in pretium nisl aliquet
+						</li>
+						<li class="list-item">
+							Nulla volutpat aliquam velit
+						</li>
+						<li class="list-item">
+							Faucibus porta lacus fringilla vel
+						</li>
+						<li class="list-item">
+							Aenean sit amet erat nunc
+						</li>
+						<li class="list-item">
+							Eget porttitor lorem
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<ul>
+						<li class="list-item">
+							Lorem ipsum dolor sit amet
+						</li>
+						<li class="list-item">
+							Consectetur adipiscing elit
+						</li>
+						<li class="list-item">
+							Integer molestie lorem at massa
+						</li>
+						<li class="list-item">
+							Facilisis in pretium nisl aliquet
+						</li>
+						<li class="list-item">
+							Nulla volutpat aliquam velit
+						</li>
+						<li class="list-item">
+							Faucibus porta lacus fringilla vel
+						</li>
+						<li class="list-item">
+							Aenean sit amet erat nunc
+						</li>
+						<li class="list-item">
+							Eget porttitor lorem
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-2">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-8">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img-top" alt="Bootstrap Thumbnail First"
+						     src="https://www.layoutit.com/img/people-q-c-600-200-1.jpg">
+						<div class="card-block">
+							<h5 class="card-title">
+								Card title
+							</h5>
+							<p class="card-text">
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta
+								gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+							<p>
+								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img-top" alt="Bootstrap Thumbnail Second"
+						     src="https://www.layoutit.com/img/city-q-c-600-200-1.jpg">
+						<div class="card-block">
+							<h5 class="card-title">
+								Card title
+							</h5>
+							<p class="card-text">
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta
+								gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+							<p>
+								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img-top" alt="Bootstrap Thumbnail Third"
+						     src="https://www.layoutit.com/img/sports-q-c-600-200-1.jpg">
+						<div class="card-block">
+							<h5 class="card-title">
+								Card title
+							</h5>
+							<p class="card-text">
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta
+								gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+							<p>
+								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-2">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-8">
+		</div>
+		<div class="col-md-2">
+		</div>
+	</div>
+</div>
+
 <!--<script src="js/jquery.min.js"></script>-->
 <!--<script src="js/bootstrap.min.js"></script>-->
 <!--<script src="js/scripts.js"></script>-->
