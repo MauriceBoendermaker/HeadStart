4f1770d274ff1bc23071b10c81281c506e8b2334
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-08-01 20:11:18 +0200

    Moved footer.php file to include directory

---
+++ include/footer.php
@@ -0,0 +1,74 @@
+<footer class="footer-01">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-lg-3 mb-4 mb-md-0">
+				<h2 class="footer-heading">headstart</h2>
+				<p>HeadStart is a powerful all-in-one Content Management System for admins and customers. It features customizable templates in an easy-to-use editor panel.</p>
+				<ul class="ftco-footer-social p-0">
+					<li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top"
+					                            title="Twitter"><span class="ion-logo-twitter"></span></a></li>
+					<li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top"
+					                            title="Facebook"><span class="ion-logo-facebook"></span></a></li>
+					<li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top"
+					                            title="Instagram"><span class="ion-logo-instagram"></span></a></li>
+				</ul>
+			</div>
+			<div class="col-md-6 col-lg-3 mb-4 mb-md-0">
+				<h2 class="footer-heading">Newest templates</h2>
+				<div class="block-21 mb-4 d-flex">
+					<a class="img mr-4 rounded" style="background-image: url(images/image_1.jpg);"></a>
+					<div class="text">
+						<h3 class="heading"><a href="#">Vanilla 526 Template is a parallax layout in an ocean blue color theme design.</a></h3>
+						<div class="meta">
+							<div><a href="#"><span class="icon-calendar"></span> Jul. 26, 2022</a></div>
+							<!--							<div><a href="#"><span class="icon-person"></span> Admin</a></div>-->
+							<!--							<div><a href="#"><span class="icon-chat"></span> 19</a></div>-->
+						</div>
+					</div>
+				</div>
+				<div class="block-21 mb-4 d-flex">
+					<a class="img mr-4 rounded" style="background-image: url(images/image_2.jpg);"></a>
+					<div class="text">
+						<h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
+						<div class="meta">
+							<div><a href="#"><span class="icon-calendar"></span> Jul. 26, 2022</a></div>
+							<!--							<div><a href="#"><span class="icon-person"></span> Admin</a></div>-->
+							<!--							<div><a href="#"><span class="icon-chat"></span> 19</a></div>-->
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-lg-3 pl-lg-5 mb-4 mb-md-0">
+				<h2 class="footer-heading">Quick Links</h2>
+				<ul class="list-unstyled">
+					<li><a href="#" class="py-2 d-block">Home</a></li>
+					<li><a href="#" class="py-2 d-block">About</a></li>
+					<li><a href="#" class="py-2 d-block">Services</a></li>
+					<li><a href="#" class="py-2 d-block">Works</a></li>
+					<li><a href="#" class="py-2 d-block">Blog</a></li>
+					<li><a href="#" class="py-2 d-block">Contact</a></li>
+				</ul>
+			</div>
+			<div class="col-md-6 col-lg-3 mb-4 mb-md-0">
+				<h2 class="footer-heading">Have a Question?</h2>
+				<div class="block-23 mb-3">
+					<ul>
+						<li><span class="icon ion-ios-pin"></span><span class="text">Rotterdam, The Netherlands</span>
+						</li>
+						<li><a href="#"><span class="icon ion-ios-call"></span><span class="text">+31 6 83546256</span></a>
+						</li>
+						<li><a href="mailto:headstart@mauriceb.nl"><span class="icon ion-ios-send"></span><span class="text">headstart@mauriceb.nl</span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="col-md-12 text-center">
+				<p class="copyright">
+					&copy; <script>document.write(new Date().getFullYear());</script>
+					All rights reserved | mauriceb.nl
+				</p>
+			</div>
+		</div>
+	</div>
+</footer>
