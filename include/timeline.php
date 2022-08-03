c00e1ba161d6ddeb80ef6698d62ae20d56599366
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-08-03 21:18:29 +0200

    Created a step-by-step timeline on the index page
    In a separate file

---
+++ include/timeline.php
@@ -0,0 +1,41 @@
+<div class="timeline-container">
+	<div class="timeline-leftbox">
+		<div class="timeline-icons">
+			<a id="dashboard"> <i class="fas fa-tachometer-alt"></i> </a>
+			<a id="profile"> <i class="fas fa-user"></i> </a>
+			<a id="settings"> <i class="fas fa-cog"></i> </a>
+			<a id="messages"> <i class="fas fa-comments"></i> </a>
+			<a id="notification"> <i class="fas fa-bell"></i> </a>
+		</div>
+	</div>
+	<div class="timeline-rightbox">
+		<div class="rb-container">
+			<ul class="timeline-rb">
+				<li ng-repeat="itembx">
+					<div class="timeline-timestamp">
+						Step 1
+					</div>
+					<div class="timeline-item-title">Register an account</div>
+				</li>
+				<li ng-repeat="itembx">
+					<div class="timeline-timestamp">
+						Step 2
+					</div>
+					<div class="timeline-item-title">Choose a template</div>
+				</li>
+				<li ng-repeat="itembx">
+					<div class="timeline-timestamp">
+						Step 3
+					</div>
+					<div class="timeline-item-title">Customize your own website</div>
+				</li>
+				<li ng-repeat="itembx">
+					<div class="timeline-timestamp">
+						Step 4
+					</div>
+					<div class="timeline-item-title">Download files,<br/> or choose a hosting plan</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
