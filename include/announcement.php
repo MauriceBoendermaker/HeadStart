062174760c00c431b8993d7e5b5b7a22d4e105b1
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-08-04 21:36:58 +0200

    HTML code for announcement banner

---
+++ include/announcement.php
@@ -0,0 +1,7 @@
+<div style="text-align: center;">
+	<div class="m-bar m-red">
+		<a class="m-microphone"><i class="material-icons" style="font-size:26px;">mic</i></a>
+		<a class="m-content">Announcement Content</a>
+		<a class="m-close" href="#"><i class="material-icons">close</i></a>
+	</div>
+</div>
